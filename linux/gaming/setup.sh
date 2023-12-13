eb9e32fac19a473d5f4c5eb17cacb26cec692750
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2023-12-13 17:42:35 +0200

    added gaming directory

---
+++ linux/gaming/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# POP OS!
+echo "WARNING: this script was ment for PopOS (would maybe work on ubuntu)"
+sudo ubuntu-drivers autoinstall
+
+echo "https://youtu.be/v9tb1gTTbJE"
+echo "https://protondb.com"
+
+
