879324059cc14b88185d4c5fc9ebd2b8feefad3e
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2023-12-16 15:55:55 +0200

    removed unnecessary code

--- linux/others/opengl/glad.sh
+++ linux/others/opengl/glad.sh
@@ -1,9 +1,5 @@
-CURRENT_DIR="$PWD"
-
 echo "Unzipping glad.zip"
 unzip -qq glad.zip -d glad
 
 sudo cp -R glad/include/* /usr/include/
 printf "Success\nNow copy the file glad.c inside the src folder to your current working directory\n"
-
-cd "$CURRENT_DIR"
