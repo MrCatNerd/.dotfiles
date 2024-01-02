b84a26f3fc6531d6ba317e998ab014bd1e423bd8
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2024-01-02 19:06:52 +0200

    I did stuff this is my repository i can do whatever i want and i aint gonna explain every single change, kinda

--- linux/others/devices_and_drivers/gpu_stuff/setup.sh
+++ linux/others/devices_and_drivers/gpu_stuff/setup.sh
@@ -3,7 +3,14 @@
 
 # POP OS!
 echo "WARNING: this script was ment for PopOS (would maybe work on ubuntu)"
+
+echo Autoinstalling drivers
 sudo ubuntu-drivers autoinstall
+echo Finished installing drivers
+
+echo Linking nvidia settings to ~
+ln -srf .nvidia-settings-rc ~/.nvidia-settings-rc
+echo Finished linking nvidia settings to ~
 
 echo "https://youtu.be/v9tb1gTTbJE"
 echo "https://protondb.com"
