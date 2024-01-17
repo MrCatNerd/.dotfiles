8855d4cbb8ef4cc413029ee091fa586427f89ae0
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2024-01-17 16:18:13 +0200

    Fixed small errors in the install scripts

--- linux/clean-env.sh
+++ linux/clean-env.sh
@@ -4,7 +4,7 @@ pushd $DOTFILES
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
 do
     echo "Removing $folder"
-    # stow -D $folder
+    stow -D $folder
 done
 
 if [ "$(dirs -c)" ]; then
--- linux/debian.sh
+++ linux/debian.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 if [[ -z $STOW_FOLDERS ]]; then
-    STOW_FOLDERS="zsh,bash,vim,git"
+    STOW_FOLDERS=".local/zsh,bash,vim,git"
 fi
 
 if [[ -z $DOTFILES ]]; then
-    DOTFILES=$HOME/.dotfiles
+    DOTFILES=$HOME/.dotfiles/linux
 fi
 
 # install
-STOW_FOLDERS="$STOW_FOLDERS" DOTFILES="$DOTFILES" $DOTFILES/linux/install.sh
+# STOW_FOLDERS="$STOW_FOLDERS" DOTFILES="$DOTFILES" $DOTFILES/install.sh
 
 # clean
-#STOW_FOLDERS="$STOW_FOLDERS" DOTFILES="$DOTFILES" $DOTFILES/linux/clean-env.sh
+STOW_FOLDERS="$STOW_FOLDERS" DOTFILES="$DOTFILES" $DOTFILES/clean-env.sh
 
