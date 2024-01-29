09684be500da5c4732e91614444fe99656fadbc0
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2024-01-29 18:19:23 +0200

    Upgraded mysql install script

--- linux/others/programming_languages/mysql.sh
+++ linux/others/programming_languages/mysql.sh
@@ -7,3 +7,31 @@ sudo apt-get install libmysqlclient21 -y
 
 sudo apt-get install mysql-server -y
 
+# If you want to know where all the installed MySQL files are located
+dpkg -L mysql-server
+
+printf "MySQL config at:\n/etc/mysql/my.cnf\n/etc/mysql/conf.d/\n"
+
+echo "Starting MySQL service"
+sudo systemctl start mysql
+
+echo "MySQL service status"
+sudo systemctl status mysql
+
+printf "MySQL server port: "
+printf netstat -tlpn | grep mysql
+print "\n"
+
+echo "Starting secure installaiton script"
+sudo mysql_secure_installation
+
+echo "--------------------------------"
+mysql --version
+echo "--------------------------------"
+
+printf "TO SET PASSWORD FOR ROOT USER:\nCREATE USER \"mrcat\"@\"localhost\" IDENTIFIED BY \"supersecretpassword\";\nTHIS MAY NOT BE REQUIRED"
+echo "type \"exit\" to quit MySQL shell"
+sudo mysql -u root -p
+
+echo "for more informatio see: https://likegeeks.com/mysql-on-linux-beginners-tutorial/#Install_MySQL_on_Linux_Ubuntu_and_CentOS"
+
